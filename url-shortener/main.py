c21ab68f042c0f559f4e4446b166e464eea7b61e
yarik <ustinovichyaroslav@gmail.com>
2025-09-19 12:36:44 -0500

    Update main app: add docs url in root func

---
+++ url-shortener/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI, Request
+
+app = FastAPI(
+    title="URL Shortener",
+)
+
+
+@app.get("/")
+def read_root(
+    request: Request,
+    name: str = "World",
+):
+    docs_url = request.url.replace(
+        path="/docs",
+        query="",
+    )
+    return {
+        "message": f"Hello {name}",
+        "docs": str(docs_url),
+    }
