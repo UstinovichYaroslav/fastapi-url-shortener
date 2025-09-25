d472aefb1e1b7f3ab554229a42d04d70704e5353
yarik <ustinovichyaroslav@gmail.com>
2025-09-25 04:12:31 -0500

    Create redirect func view

---
+++ url-shortener/api/redirect_views.py
@@ -0,0 +1,24 @@
+from typing import Annotated
+
+from fastapi import Depends, APIRouter
+from starlette.responses import RedirectResponse
+
+from .api_v1.short_urls.dependencies import prefetch_short_url
+from schemas.short_url import ShortUrl
+
+
+router = APIRouter(
+    prefix="/r",
+    tags=["Redirect"],
+)
+
+
+@router.get("/{slug}")
+@router.get("/{slug}/")
+def redirect_short_url(
+    url: Annotated[
+        ShortUrl,
+        Depends(prefetch_short_url),
+    ],
+):
+    RedirectResponse(url=url.target_url)
