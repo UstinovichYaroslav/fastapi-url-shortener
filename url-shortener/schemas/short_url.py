8627b9bce6a06f80e76c2c8006aa59b79cbe897c
yarik <ustinovichyaroslav@gmail.com>
2025-09-26 11:15:09 -0500

    update short url schemas

--- url-shortener/schemas/short_url.py
+++ url-shortener/schemas/short_url.py
@@ -1,3 +1,6 @@
+from typing import Annotated
+
+from annotated_types import Len
 from pydantic import BaseModel, HttpUrl, AnyHttpUrl
 
 
@@ -5,6 +8,17 @@ class ShortUrlBase(BaseModel):
     # target_url: HttpUrl
     target_url: AnyHttpUrl
     slug: str
+
+
+class ShortUrlCreate(ShortUrlBase):
+    """
+    Модель для создания сокращенной ссылки
+    """
+
+    slug: Annotated[
+        str,
+        Len(min_length=3, max_length=10),
+    ]
 
 
 class ShortUrl(ShortUrlBase):
